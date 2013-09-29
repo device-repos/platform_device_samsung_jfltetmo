8c332883e48e9b28b8d1dd32a1774e52eae86562
treken <chupekevin@gmail.com>
2013-09-29 14:49:12 -0400

    Remove cm references and configure for OctOS

---
+++ oct.mk
@@ -0,0 +1,15 @@
+$(call inherit-product, device/samsung/jfltetmo/full_jfltetmo.mk)
+
+# Inherit some common Oct stuff.
+$(call inherit-product, vendor/oct/config/gsm.mk)
+
+# Enhanced NFC
+ $(call inherit-product, vendor/oct/config/nfc_enhanced.mk)
+
+# Inherit some common Oct stuff.
+$(call inherit-product, vendor/oct/config/common_full_phone.mk)
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jfltetmo TARGET_DEVICE=jfltetmo BUILD_FINGERPRINT="samsung/oct_jfltespr/jfltetmo:4.3/JLS36G/user/release-keys" PRIVATE_BUILD_DESC="jfltetmo-user 4.3 JLS36G release-keys"
+
+PRODUCT_NAME := oct_jfltetmo
+PRODUCT_DEVICE := jfltetmo
